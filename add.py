5aa4a3769132db583e1da3f8c0e55b515b8caa93
AadeshIngale <aadeshingale833@gmail.com>
2025-04-15 10:49:22 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,11 @@
+# Simple addition in Python
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
